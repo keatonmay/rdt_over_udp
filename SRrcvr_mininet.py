3309b8520e85b83523cec3fa02fb4fc43b44c5aa
ArielQuintanilla <40306741+ArielQuintanilla@users.noreply.github.com>
2019-04-27 13:00:59 -0500

    Updated SR rcvr and sndr

--- SRrcvr_mininet.py
+++ SRrcvr_mininet.py
@@ -1,34 +1,64 @@
-import socket, optparse
-from common import ip_checksum
+import socket
+import optparse
 import pickle
+import hashlib
+import time
 
-def send(content,destination):
-	checksum = ip_checksum
-	sock.sendto(checksum + content, destination)
+parse = optparse.OptionParser()
+parse.add_option('-i', dest='ip', default='127.0.0.1')
+parse.add_option('-p', dest='port', default=12345)
+(options, args) = parse.parse_args()
 
-buffer = 1024
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.bind((options.ip,options.port))
+sock.settimeout(3)
 
-#MAIN
-parser = optparse.OptionParser()
-parser.add_option('-i', dest='ip', default='127.0.0.1')
-parser.add_option('-p', dest='port', type='int', default=12345)
-(options, args) = parser.parse_args()
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(options.ip, options.port)
-
-next_seq = 0
-dest = (options.ip, options.port)
+expectedseqnum=1
 
 f = open('test.txt','w')
-	
-while True:
-	data, addr = sock.recvfrom(1024)
-	
-	checksum = data[:2]
-	seq = data[2]
-	content = data[3:]
-	f.write("%s: %d\n" % (addr, data))
+f.write('BEGIN FILE\n')
+f.flush()
 
-	send("ACK" + seq, dest)
-#end
+EOF = False #boolean for whether end of file is reached
+lastpktreceived = time.time()
+
+while True:
+
+	try:
+		rcvpkt=[]
+		packet,addr= sock.recvfrom(1024)
+		rcvpkt = pickle.loads(packet)
+		c = rcvpkt[-1] #-1 gets the last item in received packets (last index is check value)
+		del rcvpkt[-1]
+		h = hashlib.md5() #hash calculate checksum
+		h.update(pickle.dumps(rcvpkt))
+		if c == h.digest(): #if c == h.digest, packets received in order
+			if(rcvpkt[0]==expectedseqnum): #recieved seq num == expected seq num?
+				if rcvpkt[1]:
+					#writing address, seq number, data
+					f.write("%s: %d : %s\n" % (addr, rcvpkt[0], rcvpkt[1:]))
+					f.flush()
+				else: #if empty, reached end of file
+					EOF = True
+				expectedseqnum = expectedseqnum + 1
+				sndpkt = [] #send packets back for ACK
+				sndpkt.append(expectedseqnum) #send back updated expected seq num
+				h = hashlib.md5()
+				h.update(pickle.dumps(sndpkt))
+				sndpkt.append(h.digest())
+				sock.sendto(pickle.dumps(sndpkt), (addr[0], addr[1]))
+
+			else: #if out of order re send ACK for last in order file received
+				sndpkt = [] #send packets back for ACK
+				sndpkt.append(expectedseqnum)
+				h = hashlib.md5()
+				h.update(pickle.dumps(sndpkt))
+				sndpkt.append(h.digest())
+				sock.sendto(pickle.dumps(sndpkt), (addr[0], addr[1]))
+		else:
+			print("ERROR")
+	except:
+		if EOF:
+			if(time.time()-lastpktreceived>3): #timeout is 3
+				break
+f.close()
--- SRsndr_mininet.py
+++ SRsndr_mininet.py
@@ -1,29 +1,63 @@
-import socket, optparse
-from common import ip_checksum
+import socket
+import optparse
+import hashlib
+import pickle
+import time
 
-buffer = 1024
-
-#MAIN
 parser = optparse.OptionParser()
 parser.add_option('-i', dest='ip', default='127.0.0.1')
 parser.add_option('-p', dest='port', type='int', default=12345)
 (options, args) = parser.parse_args()
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(options.ip, options.port)
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.settimeout(0.001)
 
-buffer = 100
-seq = 0
-dest = (options.ip, options.port)
+#initializes window variables (upper and lower window bounds, position of next seq number)
+base=1
+nextSeqnum=1
+windowSize=7
+window = []
 
-f = open("./500K.txt","rb")
-data = f.read(buffer)
-	
-while(data):
-	packet = []
-	packet.append(seq)
-	packet.append(data)
-	send_sock.sendto(str(seq), dest)
-	message, addr = sock.recvfrom(1024)
+#SENDS DATA
+f = open("./500K.txt","rb") 
+data = f.read(100)
+done = False #boolean to see if file is empty
+lastackreceived = time.time()
 
-	print message
+while not done or window: #windows is not full and data is not empty
+	if(nextSeqnum<base+windowSize) and not done:
+		sndpkt = [] #seq number, data, checksum (index 0, 1, 2)
+		sndpkt.append(nextSeqnum)
+		sndpkt.append(data)
+		h = hashlib.md5()
+		h.update(pickle.dumps(sndpkt))
+		sndpkt.append(h.digest())
+		sock.sendto(pickle.dumps(sndpkt), (options.ip, options.port))
+		nextSeqnum = nextSeqnum + 1 #send and update next seq
+		if(not data):
+			done = True #no more data to end
+		window.append(sndpkt) #add packet to window
+		data = f.read(100)
+	try:
+		packet,addr = sock.recvfrom(1024)
+		rcvpkt = [] #receive acks
+		rcvpkt = pickle.loads(packet)
+		c = rcvpkt[-1] #check value
+		del rcvpkt[-1]
+		h = hashlib.md5() #checksum calculated
+		h.update(pickle.dumps(rcvpkt))
+		if c == h.digest(): #if true, then received in order
+			#ACKS RECEIVED
+			while rcvpkt[0]>base and window:
+				lastackreceived = time.time()
+				del window[0]
+				base = base + 1
+		else:
+			print("ERROR")
+	except:
+		if(time.time() - lastackreceived > 0.01): #timout is 0.01
+			for i in window:
+				sock.sendto(pickle.dumps(i), (options.ip, options.port))
+
+f.close()    
+sock.close()
